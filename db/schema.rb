6d1a0a72f2d1e99f398d5f144b2349862ed62605
brianaalexander <bribri0283@gmail.com>
2022-09-18 15:09:56 -0500

    made rooms model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_18_195104) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_18_200801) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rooms", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.decimal "price", precision: 9, scale: 2
+    t.text "description"
+    t.string "home_type"
+    t.string "room_type"
+    t.integer "total_occupancy"
+    t.integer "total_bedrooms"
+    t.integer "total_bathrooms"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
