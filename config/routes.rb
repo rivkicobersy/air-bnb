778e8f01d910611b65735d089caca64ba950c8e6
brianaalexander <bribri0283@gmail.com>
2022-09-18 16:33:55 -0500

    added part 2 authentication

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   get "/signup" => "users#new"
   post "/users" => "users#create"
+  get "/login" => "sessions#new"
+  post "/sessions" => "sessions#create"
+  get "/logout" => "sessions#destroy"
 end
