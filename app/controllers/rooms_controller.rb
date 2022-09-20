9e7ab48a567bf83106f595fa610a8ba4258fbc1d
rivkicobersy <rcobersy@gmail.com>
2022-09-20 04:08:28 +0200

    rooms update/edit added

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -31,4 +31,26 @@ class RoomsController < ApplicationController
     @room.save
     redirect_to "/rooms"
   end
+
+  def edit
+    @room = Room.find_by(id: params[:id])
+    render template: "rooms/edit"
+  end
+
+  def update
+    @room = Room.find_by(id: params[:id])
+    @room.user_id = params[:room][:user_id]
+    @room.address = params[:room][:address]
+    @room.city = params[:room][:city]
+    @room.state = params[:room][:state]
+    @room.price = params[:room][:price]
+    @room.description = params[:room][:description]
+    @room.home_type = params[:room][:home_type]
+    @room.room_type = params[:room][:room_type]
+    @room.total_occupancy = params[:room][:total_occupancy]
+    @room.total_bedrooms = params[:room][:total_bedrooms]
+    @room.total_bathrooms = params[:room][:total_bathrooms]
+    @room.save
+    redirect_to "/rooms"
+  end
 end
