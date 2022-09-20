4cee3de086cd3aa8b887c366c1fe234bb0ee86ba
brianaalexander <bribri0283@gmail.com>
2022-09-19 19:01:10 -0500

    updates controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render template: "users/index"
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    render template: "users/show"
+  end
+
   def new
     @user = User.new
     render template: "users/new"
@@ -17,5 +27,19 @@ class UsersController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+    render template: "users/edit"
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:photo][:name]
+    @user.email = params[:user][:email]
+    @user.image = params[:user][:image]
+    @user.save
+    redirect_to "/users"
   end
 end
