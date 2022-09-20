ed22a3aeab5d63aeaf105d2fe622a1cf29b225ae
rivkicobersy <rcobersy@gmail.com>
2022-09-20 04:02:03 +0200

    rooms create added

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -8,4 +8,27 @@ class RoomsController < ApplicationController
     @room = Room.find_by(id: params[:id])
     render template: "rooms/show"
   end
+
+  def new
+    @room = Room.new
+    render template: "rooms/new"
+  end
+
+  def create
+    @room = Room.new(
+      user_id: params[:room][:user_id],
+      address: params[:room][:address],
+      city: params[:room][:city],
+      state: params[:room][:state],
+      price: params[:room][:price],
+      description: params[:room][:description],
+      home_type: params[:room][:home_type],
+      room_type: params[:room][:room_type],
+      total_occupancy: params[:room][:total_occupancy],
+      total_bedrooms: params[:room][:total_bedrooms],
+      total_bathrooms: params[:room][:total_bathrooms],
+    )
+    @room.save
+    redirect_to "/rooms"
+  end
 end
