f1f1bf5550531a85f06f793bf679410363bdd749
rivkicobersy <rcobersy@gmail.com>
2022-09-20 03:50:39 +0200

    rooms show added

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,4 +3,9 @@ class RoomsController < ApplicationController
     @rooms = Room.all
     render template: "rooms/index"
   end
+
+  def show
+    @room = Room.find_by(id: params[:id])
+    render template: "rooms/show"
+  end
 end
