2862a61bf2eaa50b4ada7b8b71a70c67516d2e2d
brianaalexander <bribri0283@gmail.com>
2022-09-20 20:54:31 -0500

    updated price for new reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -17,15 +17,22 @@ class ReservationsController < ApplicationController
   end
 
   def create
+    room = Room.find_by(id: params[:reservation][:room_id])
+    price = room.price
+    # subtotal = (params[:end_date] - [:start_date]) * price
+    # tax = 0.09 * subtotal
+    # total = tax + subtotal
+    total = price
     @reservation = Reservation.new(
       user_id: current_user.id,
       room_id: params[:reservation][:room_id],
       start_date: params[:reservation][:start_date],
       end_date: params[:reservation][:end_date],
-      price: params[:reservation][:price],
-      start_date: params[:reservation][:start_date],
+      price: total,
       total_guests: params[:reservation][:total_guests],
     )
+    @reservation.price = price * (@reservation.end_date - @reservation.start_date).to_i / 86400.0
+
     @reservation.save
     redirect_to "/reservations"
   end
